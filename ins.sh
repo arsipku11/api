c916900f2e1da86f0f84f5d02729971fdb59b825
I'm Unix <ferdi@ferdiwardiyan.eu.org>
2022-10-17 15:33:27 +0700

    Create ins.sh

---
+++ ins.sh
@@ -0,0 +1,10 @@
+apk add --no-cache \
+        python3 \
+        build-base \
+        g++ \
+        libpng \
+        libpng-dev \
+        jpeg-dev \
+        pango-dev \
+        cairo-dev \
+        giflib-dev
